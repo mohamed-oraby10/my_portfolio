a4c8ef9f8baf95204f71652b1e168a2c8b88eb4c
Mohamed Oraby Hazem <mohamedoraby0500@gmail.com>
2026-03-11 21:33:29 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2098715728"
+    serviceWorkerVersion: "199580489"
   }
 });
